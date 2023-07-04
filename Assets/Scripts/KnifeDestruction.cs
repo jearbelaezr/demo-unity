17995aee3f6fad916a644df4ef00c316ddee84a6
Jorge Arbelaez <jearbelaezr@audisoft.com>
2023-07-03 21:25:36 -0500

    Destroy knives after LifeSpan time

---
+++ Assets/Scripts/KnifeDestruction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeDestruction : MonoBehaviour
+{
+    public float lifeSpan = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
